edecee7c8a9ba5a8d52d1c69a3ba29fcd239b57e
Rishabh763 <rs0284388@gmail.com>
2024-04-10 00:13:39 +0530

    almost finished

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import data from '../data.json';
+import data from "../data.json";
 
 function Crew() {
   const [crews, setCrews] = useState([]);
@@ -17,26 +17,32 @@ function Crew() {
   const crew = crews[activeTab] || {};
 
   return (
-    <div className="crew full-width min-h-screen">
-      <Navbar/>
-      <h2 className="pt-32 pb-8 uppercase text-[16px] sm:text-[20px] md:text-[28px]"><span className="text-white/25 font-bold tracking-[4.8px]">02</span>&nbsp;  Meet your crew</h2>
-      <div className="" >
-        <div className="flex gap-2">
-          {crews.map((crewMember, index) => (
-            <button
-              key={index}
-              className={`${index === activeTab ? 'active-crew' : ''} aspect-square w-3 bg-white/40 rounded-full hover:bg-white/80 transition-colors`}
-              onClick={() => changeTab(index)}
-            >
-            </button>
-          ))}
+    <div className="crew full-width min-h-screen md:px-16">
+      <Navbar />
+      <h2 className="pt-32 pb-8 uppercase text-[16px] sm:text-[20px] md:text-[28px]">
+        <span className="text-white/25 font-bold tracking-[4.8px]">02</span>
+        &nbsp; Meet your crew
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
+        <div className="crew-content flex flex-col gap-6 md:gap-28 items-center md:items-start justify-between md:justify-self-start self-center">
+          <div className="text max-w-[70ch] flex flex-col gap-4 items-center md:items-start">
+            <p className="text-white/60 text-2xl sm:text-3xl">{crew.role}</p>
+            <p className="text-4xl sm:text-6xl tracking-tight">{crew.name}</p>
+            <p className="text-lg text-center md:text-start">{crew.bio}</p>
+          </div>
+          <div className="flex gap-2">
+            {crews.map((crewMember, index) => (
+              <button
+                key={index}
+                className={`${
+                  index === activeTab ? "active-crew" : ""
+                } aspect-square w-3 bg-white/40 rounded-full hover:bg-white/80 transition-colors`}
+                onClick={() => changeTab(index)}
+              ></button>
+            ))}
+          </div>
         </div>
-        <div className="crew-content">
-          <img src={crew.images?.webp} alt={`${crew.name} image`} />
-          <h2>{crew.name}</h2>
-          <p>{crew.role}</p>
-          <p>{crew.bio}</p>
-        </div>
+        <img className="w-72 sm:w-80 md:w-[360px] place-self-center sm:row-auto row-span-full" src={crew.images?.webp} alt={`${crew.name} image`} />
       </div>
     </div>
   );
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -19,28 +19,33 @@ function Technology() {
   return (
     <div className="technology full-width min-h-screen">
       <Navbar />
-      <h2 className="pt-32 pb-8 uppercase text-[16px] sm:text-[20px] md:text-[28px]"><span className="text-white/25 font-bold tracking-[4.8px]">03</span>&nbsp; SPACE LAUNCH 101</h2>
-      <div className="">
-        <div className="tab-buttons flex gap-4">
-          {technologys.map((technologyTab, index) => (
-            <button
-              key={index}
-              className={`${
-                index === activeTab ? "active-technology" : ""
-              } border-2 border-white/60 rounded-full aspect-square w-16 text-xl hover:border-white transition-colors`}
-              onClick={() => changeTab(index)}
-            >
-              {index + 1}
-            </button>
-          ))}
+      <h2 className="pt-32 pb-8 uppercase text-[16px] sm:text-[20px] md:text-[28px] h-fit"><span className="text-white/25 font-bold tracking-[4.8px]">03</span>&nbsp; SPACE LAUNCH 101</h2>
+      <div className="grid md:grid-cols-[1.2fr_minmax(350px,_0.8fr)] min-h-[70vh] gap-8 dm:gap-12">
+        <div className="flex flex-col md:flex-row gap-8 lg:gap-20 items-center">
+          <div className="tab-buttons flex md:flex-col items-center md:items-start gap-4">
+            {technologys.map((technologyTab, index) => (
+              <button
+                key={index}
+                className={`${
+                  index === activeTab ? "active-technology" : ""
+                } border-2 border-white/60 rounded-full aspect-square w-16 text-xl hover:border-white transition-colors`}
+                onClick={() => changeTab(index)}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+            <div className="text max-w-[70ch] flex flex-col gap-4 items-center md:items-start">
+              <p className="text-white/60 text-base sm:text-xl uppercase">the terminology...</p>
+              <p className="text-4xl sm:text-6xl tracking-tight">{technology.name}</p>
+              <p className="text-lg text-center md:text-start">{technology.description}</p>
+            </div>
         </div>
-        <div className="">
           <img
-            src={technology.images?.landscape}
+            className=" w-80 sm:w-96 md:w-[400px] lg:w-[480px]  object-cover mx-auto md:row-auto row-span-full self-center"
+            src={technology.images?.portrait}
             alt={`${technology.name} image`}
           />
-          <h2>{technology.description}</h2>
-        </div>
       </div>
     </div>
   );
